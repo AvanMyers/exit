f04e3c928bae766d7ae7f4ad5122bb4582903892
AvanMyers <smail1341@gmail.com>
2022-08-11 16:27:52 +0300

    exit

---
+++ exit/exit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "exit";
+            string enteredWord;
+            bool cycleIsOver = false;
+
+            Console.WriteLine("Данный цикл не закончится пока вы не введете 'exit'");
+
+            while (cycleIsOver == true)
+            {
+                Console.WriteLine("Продолжаем?");
+                enteredWord = Console.ReadLine();
+                if (enteredWord == exit)
+                {
+                    cycleIsOver = true;
+                    Console.WriteLine("Досвидание");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
