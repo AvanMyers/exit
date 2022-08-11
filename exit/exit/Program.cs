c323654bdcfd11f278a63186703818aa3dec63e2
AvanMyers <smail1341@gmail.com>
2022-08-11 16:58:02 +0300

    update

--- exit/exit/Program.cs
+++ exit/exit/Program.cs
@@ -14,12 +14,13 @@ namespace exit
             string enteredWord;
             bool cycleIsOver = true;
 
-            Console.WriteLine("Данный цикл не закончится пока вы не введете 'exit'");
+            Console.WriteLine($"Данный цикл не закончится пока вы не введете '{exit}' ");
 
             while (cycleIsOver == true)
             {
                 Console.WriteLine("Продолжаем?");
                 enteredWord = Console.ReadLine();
+
                 if (enteredWord == exit)
                 {
                     Console.WriteLine("Досвидание");
